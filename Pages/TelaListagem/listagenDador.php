8108bb91498468d113c1257d95bf531c59e7ac79
FaruqueBraimo <42444957+FaruqueBraimo@users.noreply.github.com>
2019-05-04 23:12:05 -0500

    day4

--- Pages/TelaListagem/listagenDador.php
+++ Pages/TelaListagem/listagenDador.php
@@ -2,8 +2,11 @@
 <html>
 <head>
  <meta charset="UTF-8">
-<link rel="icon" type="imagem/png" href="resources/foto-3.png" />
- <link rel="stylesheet" type="text/css" href="css/styleRegistro.css"/>
+<lin
+k rel="icon" type="imagem/png" href="resources/foto-3.png" />
+<link rel="stylesheet" type="text/css" href="css/styleRegistro.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
  <script src ="js/script.js"> </script>
 
  <meta  name="Author" content="Faruque">
@@ -14,32 +17,278 @@
 
 
 
+
  <body>
 
 
     <div class="registro_Dador">
 
-    <div class="container">
-            <div class="reg_info">
-              <p>Doadores</P>
+
+
+      <div class="reg_info">
+        <span style="font-size: 2rem; text-align:center" ; >
+          <span style="color: white;">
+            <i class="fas fa-user"></i>
+            Doadores
+          </span>
+
+          </div>
+
+<div class="filtros">
+  <p> Filtros </p>
+<form>
+
+  <input type="text" name="nome" id="Nome_id" placeholder="Nome" >
+  <input type="text"  name="apelido" id="Nome_id" placeholder="Grupo sanguineo" >
+  <span style="font-size: 1rem; text-align:center" ; >
+
+      <input type="submit"  name="pesquisa" id="pesquisa"  value="Ok">
+
+
+
 
 <form>
 
-            <input type="text" name="qts" id="pes" placeholder="Pesquise pelo nome">
-            <input type="submit" name="qts" id="sub" value="Ok">
 
 
-</form>
+<a  class="a"   href="#">
+
+  <span style="font-size: 1rem; text-align:center" ; >
+    <span style="color: white;">
+   <i class="fas fa-plus"></i>
+    </span>
+
+ </a>
 
 
-                </div>
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+    <div class="container">
+
+
+<table>
+
+    <thead>
+
+
+          <tr>
+
+
+            <th>
+            Nome
+
+            </th>
+
+
+            <th>
+             Idade
+            </th>
+
+
+
+
+
+            <th>
+                 grupo sanguineo
+
+
+            </th>
+
+
+
+            <th>
+                Endereço
+
+
+            </th>
+
+
+
+
+            <th>
+            Apelido
+
+            </th>
+
+
+            <th>
+    Telefone
+            </th>
+
+            <th>
+    Email
+            </th>
+
+
+
+
+            <th>
+      Accao
+            </th>
+
+
+
+          </tr>
+
+
+<tbody>
+
+  <tr>
+
+    <td> <span class="photo"> F1S </span> Faeuque</td>
+  <td>Faruque</td>
+  <td>Faruque</td>
+  <td>Faruque</td>
+  <td>Faruque</td>
+  <td>Faruque</td>
+  <td>Faruque</td>
+  <td>Faruque</td>
+
+
+  </tr>
+
+
+
+
+<tr>
+
+<td> <span class="photo"> F1S </span> Faeuque</td>
+<td>Faruque</td>
+<td>Faruque</td>
+<td>Faruque</td>
+<td>Faruque</td>
+<td>Faruque</td>
+<td>Faruque</td>
+<td>Faruque</td>
+
+</tr>
+<tr>
+
+<td> <span class="photo"> F1S </span> Faeuque</td>
+<td>Faruque</td>
+<td>Faruque</td>
+<td>Faruque</td>
+<td>Faruque</td>
+<td>Faruque</td>
+<td>Faruque</td>
+<td>Faruque</td>
+
+</tr>
+<tr>
+
+<td> <span class="photo"> F1S </span> Faeuque</td>
+<td>Faruque</td>
+<td>Faruque</td>
+<td>Faruque</td>
+<td>Faruque</td>
+<td>Faruque</td>
+<td>Faruque</td>
+<td>Faruque</td>
+
+</tr>
+
+<tr>
+
+<td> <span class="photo"> F1S </span> Faeuque</td>
+<td>Faruque</td>
+<td>Faruque</td>
+<td>Faruque</td>
+<td>Faruque</td>
+<td>Faruque</td>
+<td>Faruque</td>
+<td>Faruque</td>
+
+</tr>
+
+
+
+
+</tbody>
+
+  </thead>
+
+
+
+
+</table>
+
+
+
+
                 </div>
 
 
     </div>
 
 
+<div class="botoes">
+
+
+
+
+<a href="#" class="b">
+
+  <span style="font-size: 1rem; text-align:center" ; >
+    <span style="color: ;">
+<i class="fas fa-chevron-left"></i>
+    </span>
+
+ </a>
+
+
+
+
+
+
+ <a href="#" class="b">
+ 1
+
+  </a>
+
+  <a href="#" class="b">
+  2
+
+   </a>
+   <a href="#" class="b">
+   3
+
+    </a>
+
+
+
+ <a href="#" class="b">
+
+   <span style="font-size: 1rem; text-align:center" ; >
+     <span style="color:;">
+ <i class="fas fa-chevron-right"></i>
+     </span>
+
+  </a>
+
+
+
+
+
+
+</div>
+
  </body>
+
 
 
 
